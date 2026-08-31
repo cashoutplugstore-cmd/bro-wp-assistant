4788105545fefa6f3014cb6d8ac39b9ffdf9fbcd
cashoutplugstore-cmd <cashoutplugstore@gmail.com>
2026-08-31 03:52:35 +0300

    Add files via upload

---
+++ class-auth.php
@@ -0,0 +1,21 @@
+﻿<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+final class BRO_WPA_Auth {
+
+    public static function permission_check() {
+
+        if (!current_user_can('manage_options')) {
+            return new WP_Error(
+                'bro_forbidden',
+                'Administrator permission required.',
+                array('status' => 403)
+            );
+        }
+
+        return true;
+    }
+}
---
+++ class-rest-api.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+final class BRO_WPA_REST_API {
+
+    public static function init() {
+        add_action(
+            'rest_api_init',
+            array(__CLASS__, 'register_routes')
+        );
+    }
+
+    public static function register_routes() {
+
+        register_rest_route(
+            'bro-assistant/v1',
+            '/status',
+            array(
+                'methods'  => WP_REST_Server::READABLE,
+                'callback' => array(__CLASS__, 'status'),
+                'permission_callback' => array(
+                    'BRO_WPA_Auth',
+                    'permission_check'
+                ),
+            )
+        );
+    }
+
+    public static function status() {
+
+        return rest_ensure_response(
+            array(
+                'ok' => true,
+                'plugin' => 'BRO WP Assistant',
+                'version' => BRO_WPA_VERSION,
+                'wordpress' => get_bloginfo('version'),
+                'woocommerce' => class_exists('WooCommerce'),
+            )
+        );
+    }
+}
